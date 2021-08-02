27da1210d3660ffc3dccda721c2123c9a9916f2d
jestemAria <radmehr.krash@gmail.com>
2021-08-02 23:04:54 +0430

    GameScreen created.

---
+++ screens/GameScreen.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+import NumberContainer from '../components/NumberContainer';
+import Card from '../components/Card';
+
+const generateRandomBetween = (min, max, exclude) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+    if (rndNum === exclude) {
+        return generateRandomBetween(min, max, exclude);
+    } else {
+        return rndNum;
+    }
+}
+
+const GameScreen = props => {
+    const [currentGuess, setCorrentGuess] = useState(generateRandomBetween(1, 100, props.userChoice));
+
+    return (
+        <View style={styles.screen}>
+            <Text>Opponent's Guess</Text>
+            <NumberContainer>{currentGuess}</NumberContainer>
+            <Card style={styles.buttonContainer}>
+                <Button title="LOWER" onPress={() => { }} />
+                <Button title="HIGHER" onPress={() => { }} />
+            </Card>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center'
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 20,
+        width: 250,
+        maxWidth: '80%',
+        paddingBottom: 10
+    }
+});
+
+export default GameScreen;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -39,7 +39,7 @@ const StartGameScreen = props => {
             <Card style={styles.summaryContainer}>
                 <Text>You selected</Text>
                 <NumberContainer>{selectedNumber}</NumberContainer>
-                <Button title="START GAME"/>
+                <Button title="START GAME" onPress={() => props.onStartGame(selectedNumber)}/>
             </Card >
         );
     }
