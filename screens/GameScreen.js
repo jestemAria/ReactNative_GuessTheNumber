ae3c8d0e731a1adb3f2480fe4f0c943728e6e0c5
jestemAria <radmehr.krash@gmail.com>
2021-08-04 01:29:12 +0430

    The game now can be finished with correct guess of CPU. Also GameOverScreen created.

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { View, Text, StyleSheet, Button, Alert } from 'react-native';
 
 import NumberContainer from '../components/NumberContainer';
@@ -18,22 +18,31 @@ const generateRandomBetween = (min, max, exclude) => {
 
 const GameScreen = props => {
     const [currentGuess, setCorrentGuess] = useState(generateRandomBetween(1, 100, props.userChoice));
-
+    const [rounds, setRounds] = useState(0);
     const currentLow = useRef(1);
     const currentHigh = useRef(100);
 
+    const { userChoice, onGameOver } = props;
+
+    useEffect(() => {
+        if (currentGuess === props.userChoice) {
+            onGameOver(rounds);
+        }
+    }, [currentGuess, userChoice, onGameOver]);
+
     const nextGuessHandler = direction => {
-        if ((direction === 'lower' && currentGuess < props.userChoice) || (direction === 'higher' && currentGuess > props.userChoice)){
-            Alert.alert('Don\'t Lie!', 'You know this is wrong...', [{text:'Sorry!', style: 'cancel'}])
+        if ((direction === 'lower' && currentGuess < props.userChoice) || (direction === 'higher' && currentGuess > props.userChoice)) {
+            Alert.alert('Don\'t Lie!', 'You know this is wrong...', [{ text: 'Sorry!', style: 'cancel' }])
             return;
         }
-        if (direction === 'lower'){
+        if (direction === 'lower') {
             currentHigh.current = currentGuess;
         } else {
             currentLow.current = currentGuess;
         }
         const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
         setCorrentGuess(nextNumber);
+        setRounds(currentRounds => currentRounds + 1);
     };
 
     return (
