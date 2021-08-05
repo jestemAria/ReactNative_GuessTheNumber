bd9461df431386c73a0e557b06c3f6e995c3cb79
jestemAria <radmehr.krash@gmail.com>
2021-08-05 16:49:33 +0430

    GameOverScreen now has its own Font Style with one image added to View, locally located in /assets directory.

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Image } from 'react-native';
+import BodyText from '../components/BodyText';
+import TitleText from '../components/TitleText';
 
 const GameOverScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>The Game is Over!</Text>
-            <Text>Number of rounds: {props.roundsNumber}</Text>
-            <Text>Number was: {props.userNumber}</Text>
+            <TitleText>The Game is Over!</TitleText>
+            <View style={styles.imageContainer}>
+                <Image source={require('../assets/success.png')} style={styles.image} />
+            </View>
+            <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
+            <BodyText>Number was: {props.userNumber}</BodyText>
             <Button title="NEW GAME" onPress={props.onRestart} />
         </View>
     );
@@ -17,6 +22,19 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: 'black',
+        overflow: 'hidden',
+        marginVertical: 30
+    },
+    image: {
+        width: '100%',
+        height: '100%'
     }
 });
 
