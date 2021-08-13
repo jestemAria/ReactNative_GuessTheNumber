60c6f119bffff0343ae9ece27b48a5bee9947d6e
jestemAria <radmehr.krash@gmail.com>
2021-08-13 20:26:11 +0430

    BodyText component now also gives styles from outside.

--- components/BodyText.js
+++ components/BodyText.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
-const BodyText = props => <Text style={styles.bodyText}>{props.children}</Text>
+const BodyText = props => <Text style={{...styles.bodyText, ...props.style}}>{props.children}</Text>
 
 const styles = StyleSheet.create({
     bodyText: {
